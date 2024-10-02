1243a41c7a9d87c9575206bb3740f9158a63c9f3
wvangerwen <41104904+wvangerwen@users.noreply.github.com>
2024-10-02 11:26:35 +0200

    fix import issues, editable admin install

--- hhnk_fewspy/__init__.py
+++ hhnk_fewspy/__init__.py
@@ -1,3 +1,4 @@
+import hhnk_fewspy.api_response as api_response
 from hhnk_fewspy.api_functions import *
 from hhnk_fewspy.general_functions import clean_logs, log_arguments, replace_datashare
 from hhnk_fewspy.xml_classes import DataFrameTimeseries, XmlFile, XmlHeader, XmlTimeSeries
