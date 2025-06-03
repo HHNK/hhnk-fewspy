e02abc74a2f98af284f73f334709e7031acb383f
wvangerwen <41104904+wvangerwen@users.noreply.github.com>
2025-06-03 11:04:11 +0200

    add api test

--- tests_fewspy/test_api_functions.py
+++ tests_fewspy/test_api_functions.py
@@ -1,7 +1,9 @@
 # %%
+import datetime
+
 import pandas as pd
 
-from hhnk_fewspy import api_functions
+from hhnk_fewspy import api_functions, connect_API
 from tests_fewspy.config import FOLDER_DATA
 
 
@@ -11,6 +13,17 @@ def test_get_parameters():
     df = api_functions.get_table_as_df(table_name="parameters")
 
     assert len(df) == 96
+
+
+def test_runTask_through_API():
+    """TODO deze faalt, mag niet via API starten."""
+    pi = connect_API.connect_rest()
+    pi.runTask(
+        workflowId="Import_TMX_from_file",
+        startTime=datetime.datetime.now(),
+        endTime=datetime.datetime.now(),
+        userId="Gerwen, Wietse van",
+    )
 
 
 if __name__ == "__main__":
